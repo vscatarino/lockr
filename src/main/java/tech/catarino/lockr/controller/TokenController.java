ff652e323c472c4357a97e20df5d39c076676fb4
vscatarino <vcatarino@outlook.com>
2025-06-13 18:11:16 -0300

    create controller listUsers

--- src/main/java/tech/catarino/lockr/controller/TokenController.java
+++ src/main/java/tech/catarino/lockr/controller/TokenController.java
@@ -1,6 +1,8 @@
 package tech.catarino.lockr.controller;
 
 import java.time.Instant;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import tech.catarino.lockr.controller.dto.LoginRequest;
 import tech.catarino.lockr.controller.dto.LoginResponse;
+import tech.catarino.lockr.entities.Role;
 import tech.catarino.lockr.repository.UserRepository;
 
 @RestController
@@ -33,24 +36,30 @@ public class TokenController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-       var user = userRepository.findByUsername(loginRequest.username());
-       if(user.isEmpty() || !user.get().isLoginCorrect(loginRequest, bCryptPasswordEncoder)){
-        throw new BadCredentialsException("Authentication failed. Please check your credentials.");
-       }
+        var user = userRepository.findByUsername(loginRequest.username());
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, bCryptPasswordEncoder)) {
+            throw new BadCredentialsException("Authentication failed. Please check your credentials.");
+        }
 
-       var now = Instant.now();
-       var expiresIn = 300L;
+        var now = Instant.now();
+        var expiresIn = 300L;
 
-       var claims = JwtClaimsSet.builder()
-       .issuer("mybackend")
-       .subject(user.get().getUserId().toString())
-       .issuedAt(now)
-       .expiresAt(now.plusSeconds(expiresIn))
-       .build();
+        var scopes = user.get().getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(""));
 
-       var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        var claims = JwtClaimsSet.builder()
+                .issuer("mybackend")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
+                .build();
 
-       return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
     }
 
 }
--- src/main/java/tech/catarino/lockr/controller/UserController.java
+++ src/main/java/tech/catarino/lockr/controller/UserController.java
@@ -1,10 +1,13 @@
 package tech.catarino.lockr.controller;
 
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +51,13 @@ public class UserController {
         userRepository.save(user);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAuthority('SCOPE_admin')")
+    public ResponseEntity<List<User>> listUsers(){
+        var users = userRepository.findAll();
+        return ResponseEntity.ok(users);
     }
 
 }
